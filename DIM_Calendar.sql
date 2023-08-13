84bca435b284650be659014d7cd9ea6e6a3a0947
Agustin Musanti <138232526+AgustinMusanti@users.noreply.github.com>
2023-08-12 23:31:36 -0300

    Add files via upload

---
+++ DIM_Calendar.sql
@@ -0,0 +1,25 @@
+SELECT  
+[DateKey],
+[FullDateAlternateKey] AS Date,
+--[DayNumberOfWeek]
+[EnglishDayNameOfWeek] AS Day,
+--,[SpanishDayNameOfWeek]
+--,[FrenchDayNameOfWeek]
+--,[DayNumberOfMonth]
+--,[DayNumberOfYear]
+[WeekNumberOfYear] AS WeekNr,
+[EnglishMonthName] AS Month,
+LEFT([EnglishMonthName],3) AS MonthShort,
+--,[SpanishMonthName]
+--,[FrenchMonthName]
+[MonthNumberOfYear] AS MonthNr,
+[CalendarQuarter] AS Quarter,
+[CalendarYear] AS Year 
+--[CalendarSemester]
+--,[FiscalQuarter]
+--,[FiscalYear]
+--,[FiscalSemester]
+FROM 
+[dbo].[DimDate]
+WHERE CalendarYear >= 2019
+--Limpieza de datos a partir de la tabla dbo.DimDate
